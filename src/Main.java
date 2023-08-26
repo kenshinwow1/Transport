4f7536db645fc7903315984ada7531a805ea8dba
kenshinwow1 <kenshinwow1@gmail.com>
2023-08-26 14:57:34 +0300

    transport_parth2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Transport
+        Transport bmw = new Transport (50.5f, 2500,"White", new byte[] {0, 0, 0});
+
+
+      Transport  truck = new Transport(5600, new byte[] {100,0,100});
+//        truck.speed = 140.5f;
+//        truck.weight = 5600;
+//        truck.color = "Red";
+//        truck.coordinate = new byte[] {100, 0, 100};
+//
+//        String res2 = truck.getValues();
+//        System.out.print(res2);
     }
 }
--- src/Transport.java
+++ src/Transport.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Transport {
+import java.awt.*;
+
+public class Transport {
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
+
+    public Transport(float _speed, int _weight, String _color,byte[] _coordinate){
+        System.out.println("Object : ");
+        setValues(_speed, _weight,_color, _coordinate);
+        System.out.println(getValues());
+    }
+
+    public Transport(int weight,byte[] coordinate){
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(this.getValues());
+    }
+
+
+    public void setValues(float speed, int weight, String color, byte[] coordinate){
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
+    }
+    public String getValues(){
+        String info =  "Object speed: " + this.speed + ".Weight: " + this.weight + ".Color: " + this.color+".\n";
+        String infoCoordinates = "Coordinates:\n";
+        for(int i = 0; i < coordinate.length; i++)
+            infoCoordinates += coordinate[i] + "\n";
+
+
+        return info + infoCoordinates;
+
+    }
 }
